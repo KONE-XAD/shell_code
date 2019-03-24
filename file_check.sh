e1361a663afacaaf04db5a060db7f5a4be20d9ee
dev <dev@xadocker.cn>
2019-03-24 21:15:39 +0800

    add new file file_check.sh

---
+++ file_check.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+# Author: xad
+# Descri: check file size or dir size an files's num
+
+function dir_size(){
+    du -s $1 | awk '{print $1*1024*1024}'
+}
+
+function file_nums(){
+    ls -l  $1 | grep ^- | wc -l    
+}
+
+case $1 in
+    dir_size)
+        dir_size $2
+        ;;
+    file_nums)
+        file_nums $2
+        ;;
+    *)
+        echo "Usage: `basename $0` [dir_size|file_nums]"
+esac
